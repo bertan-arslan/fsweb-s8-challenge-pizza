abd799c8c42af303a136f0d35eabd40a8337cc54
bertan-arslan <bertanars97@gmail.com>
2025-07-24 19:21:33 +0300

    img url

--- src/components/Info/InfoFoodList.js
+++ src/components/Info/InfoFoodList.js
@@ -6,7 +6,7 @@ const foodList = [
     comment: 200,
     info: "Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.",
     bannerImgUrl: "/images/foods/position-absolute-pizza-banner.png",
-    imgUrl: 'public/images/foods/position-absolute-pizza.png',
+    imgUrl: "/images/foods/position-absolute-pizza.png",
   },
   {
     name: "Terminal Pizza",
@@ -15,7 +15,7 @@ const foodList = [
     comment: 200,
     info: "Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.",
     bannerImgUrl: "",
-    imgUrl: "public/images/foods/terminal-pizza.png",
+    imgUrl: "/images/foods/terminal-pizza.png",
   },
   {
     name: "useEffect Tavuklu Burger",
@@ -24,7 +24,7 @@ const foodList = [
     comment: 200,
     info: "Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.",
     bannerImgUrl: "",
-    imgUrl: "public/images/foods/useEffect-tavuklu-burger.png",
+    imgUrl: "/images/foods/useEffect-tavuklu-burger.png",
   },
 ];
 
