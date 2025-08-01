a588f50b008ac705d4caed91d8a6c582d7323a5f
bertan-arslan <bertanars97@gmail.com>
2025-08-01 23:17:01 +0300

    responsive design edits

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ export default function Logo() {
     <img
       src={logoSrc}
       alt="Logo"
-      className="h-auto pt-8 pb-8 mx-auto"
+      className="h-auto pt-8 pb-8 mx-auto max-w-[90vw]"
     />
   );
 }
